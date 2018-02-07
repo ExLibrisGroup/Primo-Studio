b17ebb9ca83b4af375296afdbfb8fe9fe0a1ccbc
shoulm <shoulma@gmail.com>
2018-02-07 12:10:14 +0200

    add --dev option for webpack watch during development

--- gulp/tasks/bundleJs.js
+++ gulp/tasks/bundleJs.js
@@ -6,12 +6,15 @@ const webpackStream= require('webpack-stream');
 const webpack= require('webpack-stream').webpack;
 const UglifyJsPlugin= require('uglifyjs-webpack-plugin');
 const path= require('path');
+const minimist = require('minimist');
 
 gulp.task('bundle-js', function() {
     return bundleJs();
 });
 
 function bundleJs(){
+    var options = minimist(process.argv.slice(2));
+    console.log(options);
     let bundleConfig={
         entry: './primo-explore/www/renderer.js',
         resolve:{
@@ -21,11 +24,19 @@ function bundleJs(){
             filename: 'bundle.js'
         },
         target: 'web',
-        devtool: 'source-map',
-        plugins: [new UglifyJsPlugin({sourceMap: true})]
+        devtool: 'source-map'
     };
 
-    return webpackStream(bundleConfig)
+    if (options.dev){
+        console.log('webpack watching!');
+        bundleConfig['watch'] = true;
+    }
+
+    else{
+        bundleConfig['plugins'] = [new UglifyJsPlugin({sourceMap: true})];
+    }
+
+    webpackStream(bundleConfig)
         .pipe(gulp.dest('./primo-explore/www/'));
 }
 
